a773fb554d0a511a76ad6d2553c8628d1175f802
evgeniy gazetdinov <zplacesound@gmail.com>
2020-04-15 09:23:07 +0300

    For merge develop (#5)

--- apps/photoapp/serializers.py
+++ apps/photoapp/serializers.py
@@ -5,9 +5,37 @@ import os
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
+import shutil
+
+
+
+class CountViewsPhotoSerializer(serializers.ModelSerializer):
+    views = serializers.SerializerMethodField(method_name='get_list_views')
+    links = serializers.SerializerMethodField(method_name='get_unique_link_for_image') 
+    def get_list_views(self, obj):
+        pass
+    
+    def get_unique_link_for_image(self, obj):
+        pass
 
 
 class FileSerializer(serializers.ModelSerializer):
+    created_date = serializers.SerializerMethodField(method_name='time_format')
+    id = serializers.IntegerField(required=False)
+    user = serializers.SerializerMethodField(method_name='get_user')
+   
+
+    
+    def get_photo_url(self, car):
+        request = self.context.get('request')
+        photo_url = car.photo.url
+        return request.build_absolute_uri(photo_url)
+
+
+    def time_format(self,obj):
+        return  obj.created_date.strftime("%Y-%m-%d %H:%M")
+
+
     def get_user(self,obj):
         #iterate thoght photouser
         all = (obj.user.all())
@@ -16,24 +44,34 @@ class FileSerializer(serializers.ModelSerializer):
             res.append(image.user.username)
         return res
 
-    user = serializers.SerializerMethodField(method_name='get_user')
     def validate(self, data):
         image = data['image']
         if str(image.name).lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
             if image.image:
                 if image.size != 0:
                     return data
-        
+    def move_to_user(self,path_from_move,need_path,image):
+        if not os.path.exists(need_path):
+            os.mkdir(need_path)
+            shutil.move(path_from_move,need_path+image.name)
+        else:
+            shutil.move(path_from_move,need_path+image.name)        
 
 
-    def create(self, validated_data):
+    def create(self,validated_data):
         #TODO SAVE HERE
         image = validated_data.pop('image')
         current_user = self.context['user']
+        request = self.context.get('request')
         photo = Photo.objects.create(image=image)
         photo.user.add(current_user)
+        need_path = (os.getcwd()+'/media/'+str(current_user.user.username)+'/')
+        path_now = os.path.abspath(image.name)
+        path_from_move = os.path.dirname(path_now)+'/media/'+image.name
+        self.move_to_user(path_from_move,need_path,image)
+        #TODO UPDATE PATH
         return photo
 
     class Meta:
         model = Photo
-        fields = ('image','user')
+        fields = ('id', 'image', 'user', 'created_date')

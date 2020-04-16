205de5f19e29f854e08207b07285f831a50c6a49
evgesha <zplacesound@gmail.com>
2020-04-16 19:17:50 +0300

    fixes for add user

--- apps/userapp/serializers.py
+++ apps/userapp/serializers.py
@@ -24,15 +24,16 @@ class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source="user.username")
     is_admin = serializers.CharField(source="user.is_staff",required=False)
     photos = serializers.SerializerMethodField(method_name='get_images')
-    
-    def create(self, validated_data):
-        user = PhotoUser.objects.create(
-            user__username=validated_data['username']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
+  
 
-        return user
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['user']['username']
+        )
+        user.set_password(validated_data['user']['password'])
+        user.save()
+        photo= PhotoUser(user=user)
+        return photo
     
     def update(self, instance, validated_data):
         if 'password' in validated_data:
@@ -40,9 +41,15 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         return super(UserSerializer, self).update(instance, validated_data)
 
+        
+    def validate(self,data):
+        is_already_exists = User.objects.filter(username=data['user']['username']).exists()
+        if is_already_exists:
+            raise serializers.ValidationError('already exists')
+        return data
+
     class Meta:
         model = User
-    
         fields = ('id','username','is_admin','photos','password')
   
 
